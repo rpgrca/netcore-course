2cef630228cd355d626b40b93296fd6e0a979705
Roberto <rpgrca@gmail.com>
2019-08-15 20:32:02 -0300

    Added second test, fixed class to work with it

--- 190815TDD/Domain/Division.cs
+++ 190815TDD/Domain/Division.cs
@@ -1,7 +1,7 @@
 namespace TDD.Domain {
     public class Division {
         public double Dividir(int v1, int v2) {
-            return 1;
+            return v1 / (double)v2;
         }
     }
 }
--- 190815TDDUnitTest/DivisionTest.cs
+++ 190815TDDUnitTest/DivisionTest.cs
@@ -7,12 +7,20 @@ namespace TDDUnitTest
     public class DivisionTest
     {
         [Fact]
-        public void Given1And1_DivisionShouldReturn1()
+        public void GivenTwoEqualNumbers_DivisionShouldReturnOne()
         {
             var division = new Division();
             var resultado = division.Dividir(1, 1);
 
             Assert.Equal(1, resultado);
+        }
+
+        [Fact]
+        public void GivenOneAndTwo_DividirShouldReturnCorrectValue() {
+            var division = new Division();
+            var resultado = division.Dividir(1, 2);
+
+            Assert.Equal(0.5, resultado);
         }
     }
 }
