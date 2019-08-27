87c492683a953cef16b3e973ce717a9b6e66f584
Roberto <rpgrca@gmail.com>
2019-08-27 19:59:57 -0300

    Add test project for lambdas

---
+++ 190827ParImparLambda/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ParImparLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, bool> IsOdd = x => ((x & 1) == 1);
+            int value = new Random().Next();
+            string type = IsOdd(value)? "impar" : "par";
+
+            Console.WriteLine($"El numero {value} es {type}.");
+        }
+    }
+}
