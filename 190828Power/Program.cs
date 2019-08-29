97cbd66e69d0b04951acfb3a9a813c7b1a39688e
Roberto <reybrujo@gmail.com>
2019-08-28 23:13:43 -0300

    New project, lambda example

---
+++ 190828Power/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = new Random().Next(1, 10);
+            Func<int, int> cuadrado = x => x * x;
+            Console.WriteLine($"El cuadrado de {value} es {cuadrado(value)}.");
+        }
+    }
+}
